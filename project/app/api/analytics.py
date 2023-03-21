e24c020e21cbbd709bea4355a853edbb781bf5f6
yevhenii.ravliuk <yevhenii.ravliuk@jetsoftpro.com>
2023-03-21 16:14:52 +0200

    Add flake8 config

--- project/app/api/analytics.py
+++ project/app/api/analytics.py
@@ -10,7 +10,7 @@ analytic_router = APIRouter()
 
 
 @analytic_router.get("/analytics/", response_model=list[Post])
-async def get_analytics(session: Session, date_from: date = Query(...), date_to: date = Query(...)):
+async def get_analytics(user: CurrentUser, session: Session,  date_from: date = Query(...), date_to: date = Query(...)):
     result = await session.execute(select(Post).where(
         and_(Post.created_at >= date_from, Post.created_at <= date_to)
     ).order_by(
